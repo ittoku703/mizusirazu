e273b404a2013698175ce17e868bf02fbb03db87
shinzanmono <shinzanmono1192@gmail.com>
2021-10-12 03:39:14 +0900

    add sorcery user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  NAME_REGEXP = /\A[\w+\-.\s]+\z/i
+  EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true, format: { with: EMAIL_REGEXP }, length: { maximum: 255 }, uniqueness: true
+  with_options if: -> { new_record? || changes[:crypted_password] } do |user|
+    user.validates :password, length: { minimum: 3 }, confirmation: true
+    user.validates :password_confirmation, presence: true
+  end
 end
