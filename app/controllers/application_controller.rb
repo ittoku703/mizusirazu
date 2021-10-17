98b4493a1b3ffd72fff2bba0013bce9af84cf9f9
shinzanmono <shinzanmono1192@gmail.com>
2021-10-18 01:54:53 +0900

    redirect to MYDOMAIN to herokuapp.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_domain
+  FQDN = 'www.mizusirazu.net'
+
   def hello
     render template: 'hello'
   end
@@ -11,5 +14,15 @@ class ApplicationController < ActionController::Base
 
   def already_logged_in
     redirect_to root_path, notice: 'You are already logged in' if logged_in?
+  end
+
+  private
+
+  # redirect to MYDOMAIN to herokuapp.com
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+    # for the test
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
   end
 end
