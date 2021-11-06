2b2e32bf03ae21f87f0db96e4cb674371e94c9ab
shinzanmono <shinzanmono1192@gmail.com>
2021-11-06 23:52:57 +0900

    add forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery prepend: true
   before_action :ensure_domain
   before_action :set_locale
   # before_action :configure_permitted_parameters, if: :devise_controller?
