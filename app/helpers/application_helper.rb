9dcb7f8db13b85d6535272a31649a2987e0bcf4e
shinzanmono <shinzanmono1192@gmail.com>
2021-11-16 22:33:27 +0900

    add header_link_to methods to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def header_link_to(name, path, **options)
+    options[:class] = 'block mt-2 sm:inline-block sm:mr-4 sm:mt-0'
+    link_to name, path, options
+  end
 end
