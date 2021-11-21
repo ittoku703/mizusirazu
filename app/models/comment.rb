09685e91dabc9157e3b9f23c079c9d2b8177d30d
shinzanmono <shinzanmono1192@gmail.com>
2021-11-22 02:20:08 +0900

    add images to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :micropost, class_name: 'Micropost'
 
+  has_many_attached :images
+
   validates :micropost_id, presence: true
   validates :content, presence: true, length: { maximum: 1000 }
+  validates :images, content_type: %i[png jpeg jpg gif],
+                     size: { less_than: 5.megabytes },
+                     limit: { max: 3 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -25,4 +25,34 @@ RSpec.describe Comment, type: :model do
       expect(valid).to eq false
     end
   end
+
+  describe 'images' do
+    it 'attached? is true' do
+      uploaded_files = [
+        fixture_file_upload('spec/factories/images/test.png'),
+        fixture_file_upload('spec/factories/images/test.gif')
+      ]
+      comment.images = uploaded_files
+      expect(valid).to eq true
+    end
+
+    it 'is false if the file extension is not png, gif, jpg' do
+      comment.images = [fixture_file_upload('spec/factories/images/test.dds')]
+      expect(valid).to eq false
+    end
+
+    it 'is false if the file size is 5 megabytes more' do
+      comment.images = [fixture_file_upload('spec/factories/images/ten_megabytes.png')]
+      expect(valid).to eq false
+    end
+
+    it 'is false if the 4 files selcted' do
+      uploaded_files = []
+      4.times do
+        uploaded_files << fixture_file_upload('spec/factories/images/test.gif')
+      end
+      comment.images = uploaded_files
+      expect(valid).to eq false
+    end
+  end
 end
