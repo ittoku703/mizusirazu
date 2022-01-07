d9a0c3cf0a26c7b86379d33fff6ca63e044c92ee
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-08 03:35:13 +0900

    add html title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # returns full title for each page
+  def full_title(page_title = '')
+    base_title = 'Mizusirazu'
+
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
