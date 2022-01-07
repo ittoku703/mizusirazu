339a1bbb2cd1a651afdbeee197a0a1a677ae604e
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-08 03:34:03 +0900

    add render_not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  # if resource is not found, render 404 page
+  def render_not_found(error = nil)
+    logger.info "Rendering 404 with exception: #{error.message}" if error
+    render file: Rails.root.join('public', '404.html'), status: :not_found, layout: false
+  end
 end
