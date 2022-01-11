414800b33a523d89c193adbaa2490cdc041bde67
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-11 15:15:58 +0900

    updated signup views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,15 @@ module UsersHelper
   def user_destroy_link(user)
     link_to 'Delete', user, data: { turbo_method: :delete, turbo_confirm: 'Are you sure?' }
   end
+
+  def user_form_field(form, field_name)
+    content_tag(:div) do
+      form.label(field_name, class: 'black mb-1 text-gray-600 font-semibold') +
+      form.text_field(field_name, placeholder: field_name, class: 'bg-indigo-50 px-4 py-2 outline-none rounded-md w-full')
+    end
+  end
+
+  def user_form_submit(form, text)
+    form.submit text, class: 'mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide'
+  end
 end
