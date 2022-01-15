3616cb4cafbdb53ef6fc520e97087a49f1f5eac7
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-15 17:26:46 +0900

    rename UsersHelper/user_terminal_field to ApplicationHelper/set_form_field_name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
       page_title + ' | ' + base_title
     end
   end
+
+  private
+    def set_form_field_name(field_name)
+      case(field_name)
+      # user
+      when :name then 'text' + '_field'
+      when :email then 'email' + '_field'
+      when :password then 'password' + '_field'
+      when :password_confirmation then 'password' + '_field'
+      # profile
+      when :bio then 'text' + '_area'
+      when :location then 'text' + '_field'
+      end
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,29 +3,19 @@ module UsersHelper
     link_to 'Delete', user, data: { turbo_method: :delete, turbo_confirm: 'Are you sure?' }
   end
 
-  def user_terminal_form_field(form, field_name)
-    send_field_name = set_send_field_name(field_name)
+  def user_form_field(form, field_name)
+    send = set_form_field_name(field_name)
 
     content_tag(:div) do
       form.label("#{field_name}: ") +
-      form.send(send_field_name, field_name, placeholder: field_name, class: 'p-0 border-0 border-b border-gray-800 bg-inherit text-sm')
+      form.send(send, field_name, placeholder: field_name, class: 'p-0 border-0 border-b border-gray-800 bg-inherit text-sm')
     end
   end
 
-  def user_terminal_field(user, attribute_name)
+  def user_field(user, attribute_name)
     content_tag(:p) do
       content_tag(:strong, "#{attribute_name.capitalize}: ") +
       content_tag(:span, user.send(attribute_name))
     end
   end
-
-  private
-    def set_send_field_name(field_name)
-      case(field_name)
-      when :name then 'text' + '_field'
-      when :email then 'email' + '_field'
-      when :password then 'password' + '_field'
-      when :password_confirmation then 'password' + '_field'
-      end
-    end
 end
