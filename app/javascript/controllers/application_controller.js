de9a97a04fcfe2961f951af8fe04f412787abe67
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-16 22:36:09 +0900

    fixed view of flash messages

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,17 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['userBtn', 'userList'];
+  static targets = ['userBtn', 'userList', 'flashMessages'];
 
   pageClick() {
-    const userList = this.userListTarget
+    const userList = this.userListTarget;
     userList.classList.add('hidden');
   }
 
   headerUserToggle() {
-    const userList = this.userListTarget
+    const userList = this.userListTarget;
     setTimeout(function () {
-      userList.classList.toggle('hidden');
+      userList.classList.remove('hidden');
     }, 10);
+  }
+
+  flash_messages_close() {
+    const flashMessages = this.flashMessagesTarget;
+    flashMessages.classList.add('hidden');
   }
 }
