620e64446520c2f8e59637845947486137261733
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-17 18:57:39 +0900

    fixed view of the users list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -23,4 +23,30 @@ module UsersHelper
       content_tag(:span, user.send(attribute_name))
     end
   end
+
+  def user_index_tag(user, attribute_name, options = {})
+    options[:class].to_s << ' truncate'
+    content_tag(:div, user.send(attribute_name), options)
+  end
+
+  def user_index_profile_tag(user_profile, attribute_name, options = {})
+    options[:class].to_s << ' max-w-lg truncate'
+    content_tag(:div, user_profile.send(attribute_name), options)
+  end
+
+  def user_index_control_tag(user, options = {})
+    options[:class] = 'block'
+    if current_user?(user)
+      link_to('Edit', edit_user_path(user), options) +
+      user_destroy_link(user, options)
+    else
+      content_tag(:div, 'Access denied')
+    end
+  end
+
+  def user_index_image_tag(user)
+    link_to(user_path(user)) do
+      image_tag('image-not-found.png', class: 'w-12 h-12 bg-white rounded-full')
+    end
+  end
 end
