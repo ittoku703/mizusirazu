4d9c9ae212e6a1db85c607582ef2a04984c1a656
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-18 22:44:18 +0900

    add user edit test (rspec)

---
+++ spec/features/user_edit_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature 'UserEdit', type: :feature do
+  let(:user) { create(:user) }
+
+  scenario 'valid user settings' do
+    log_in_as(user)
+    visit edit_user_path
+    fill_in 'Name', with: 'update_user'
+    fill_in 'Email', with: user.email
+    click_button 'Update user'
+    expect(page).to have_selector 'div#notice'
+    expect(page).to have_content 'update_user'
+  end
+
+  scenario 'invalid user settings' do
+    log_in_as(user)
+    visit edit_user_path
+    fill_in 'Name', with: 'hoge'
+    fill_in 'Email', with: 'bar'
+    click_button 'Update user'
+    expect(page).to have_selector 'div#error_explanation'
+  end
+
+  scenario 'non logged in user settings' do
+    visit edit_user_path
+    expect(page).to have_selector 'div#alert'
+  end
+end
