70ac7d057222f5e64dfc6a0015d74b00c9a778da
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-01-25 14:58:38 +0900

    fixed sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,20 @@ class SessionsController < ApplicationController
   # POST /sessions
   def create
     @user = User.where(name: params[:session][:name_or_email]).or(User.where(email: params[:session][:name_or_email])).take
-    respond_to do |format|
-      if @user&.authenticate(params[:session][:password])
-        log_in @user
-        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-        format.html { redirect_back_or(@user, notice: 'successfully user was logged in') }
-      else
-        flash.now[:alert] = 'Could not login. Please try again'
-        format.html { render :new, status: :unprocessable_entity }
-      end
+      respond_to do |format|
+        if @user&.authenticate(params[:session][:password])
+          if @user.activated?
+            log_in @user
+            params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+            format.html { redirect_back_or(@user, notice: 'successfully user was logged in') }
+          else
+            flash[:alert] = 'This user has not activated yet. Please user activate'
+            format.html { redirect_to new_account_activation_path }
+          end
+        else
+          flash.now[:alert] = 'Could not login. Please try again'
+          format.html { render :new, status: :unprocessable_entity }
+        end
     end
   end
 
