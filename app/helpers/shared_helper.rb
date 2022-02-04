d13e43da296596e4af8b929b56cb52bcd71a29cf
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-02-04 23:35:41 +0900

    fixed settings view

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -1,14 +1,28 @@
 module SharedHelper
+  def settings_link_to(link, text, options = {})
+    options[:class] = "#{options[:class]} block p-2 border-b border-gray-800 hover:text-sky-500"
+
+    link_to(link, options) do
+      link_icon(text) +
+      content_tag(:span, text, class: 'ml-2')
+    end
+  end
+
   def settings_form_field(form, field_name, span_text = '', options = {})
     send_name = set_form_field_name(field_name)
     options[:placeholder] = field_name
     options[:class] = "#{options[:class]} w-full md:w-64 block text-sm border border-gray-300 shadow rounded"
 
     content_tag(:div) do
-      form.label("#{field_name}", class: 'block text-gray-600 pl-2') +
+      form.label("#{field_name}", class: 'block text-gray-800 pl-2') +
       form.send(send_name, field_name, options) +
-      content_tag(:span, span_text, class: 'text-xs text-gray-500 italic tracking-tight')
+      content_tag(:span, span_text, class: 'text-xs text-gray-800 italic tracking-tight')
     end
+  end
+
+  def settings_form_submit(form, text, options = {})
+    options[:class] = "#{options[:class]} p-2 border rounded border-black shadow bg-green-500 hover:bg-green-400 text-white"
+    form.submit(text, options)
   end
 
   def send_email_form_field(form, field, options = {})
@@ -28,4 +42,12 @@ module SharedHelper
 
     form.submit(text, options)
   end
+
+  private
+    def link_icon(text)
+      case text
+      when 'Account'  then fa_stacked_icon('user', base: 'circle-thin')
+      when 'Profile' then fa_stacked_icon('pencil', base: 'circle-thin')
+      end
+    end
 end
