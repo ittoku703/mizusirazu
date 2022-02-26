ccb2d884f03968ce0350ff9d4938fa31d6017dee
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-02-27 02:10:21 +0900

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,11 +36,14 @@ Provider.find_or_create_from_auth({
   :uid => "123456",
   :info => {
     :name => "John Q Public",
+    :email => "john@example.com",
     :image => "http://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
+    :description => "Hello I\'m Jogn !",
+    :location => 'US'
   },
   :extra => {
     :raw_info => {
-      :screen_name => "",
+      :screen_name => "john_q_public_12345",
     }
   }
 })
