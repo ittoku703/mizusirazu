cd87c75b3368b3a37f3c90f7121a5fa56efc4810
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-03-11 00:24:05 +0900

    add provider github views

--- app/helpers/provider_helper.rb
+++ app/helpers/provider_helper.rb
@@ -1,11 +1,21 @@
 module ProviderHelper
   def twitter_omniauth_button_to(options = {})
-    options[:class] = "#{options[:class]} px-4 py-1 text-sky-500 border border-amber-500 bg-white rounded shadow hover:text-black hover:bg-sky-500"
+    options[:class] = "#{options[:class]} px-4 py-1 text-sky-500 bg-white border border-amber-500 rounded shadow hover:text-black hover:bg-sky-500"
     options[:data] = { turbo: false }
     options[:alt] = :twitter
 
     button_to('/auth/twitter', options) do
       fa_icon('twitter')
+    end
+  end
+
+  def github_omniauth_button_to(options = {})
+    options[:class] = "#{options[:class]} px-4 py-1 text-black bg-white border border-emerald-500 rounded shadow hover:text-white hover:bg-gray-700"
+    options[:data] = { turbo: false }
+    options[:alt] = :github
+
+    button_to('/auth/github', options) do
+      fa_icon('github')
     end
   end
 end
--- spec/features/provider_login_spec.rb
+++ spec/features/provider_login_spec.rb
@@ -8,6 +8,13 @@ RSpec.feature "ProviderLogins", type: :feature do
     expect(page).to have_selector 'div#notice'
   end
 
+  scenario 'github login' do
+    github_mock()
+    visit new_session_path()
+    find('button[alt="github"]').click
+    expect(page).to have_selector 'div#notice'
+  end
+
   scenario 'twitter login failed' do
     twitter_invalid_credentials()
     visit new_session_path()
