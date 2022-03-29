9ddcdf37071f68274ad0c05251b01dfc774adc28
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-03-30 00:38:37 +0900

    add I18n to Micropost views

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,33 +1,27 @@
 module MicropostsHelper
-  def micropost_form_field(form, field_name, options = {})
-    send = get_micropost_form_field_name(field_name)
-    options[:class] = "#{options[:class]} w-full text-md"
-    options[:placeholder] = field_name
+  def micropost_form_field(form, field, text, options = {})
+    send = get_micropost_form_field_name(field)
+    options[:class] = "w-full text-md #{options[:class]}"
 
     content_tag(:div) do
-      form.send(send, field_name, options)
+      form.label(text) +
+      form.send(send, field, options)
     end
   end
 
   def micropost_form_submit(form, text, options = {})
-    options[:class] = "#{options[:class]} text-sm p-2 bg-emerald-500 border border-gray-500 rounded shadow text-white hover:bg-emerald-400"
+    options[:class] = "text-sm p-2 bg-emerald-500 border border-gray-500 rounded shadow text-white hover:bg-emerald-400 #{options[:class]}"
 
     form.submit(text, options)
   end
 
-  def micropost_destroy_link(micropost, options = {})
-    options[:data] = { turbo_method: :delete, turbo_confirm: 'Are you sure?' }
-
-    link_to('Delete Micropost', micropost, options)
-  end
-
   private
 
-  def get_micropost_form_field_name(name)
-    case name
-    when :title     then 'text_field'
-    when :content      then 'text_area'
-    when :images   then 'file_field'
+  def get_micropost_form_field_name(field)
+    case field
+    when :title   then 'text_field'
+    when :content then 'text_area'
+    when :images  then 'file_field'
     end
   end
 end
