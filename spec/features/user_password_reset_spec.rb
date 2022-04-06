b75f01b000f6c126fa9dd880b256b6002291cd45
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-04-06 18:22:57 +0900

    updated PasswordReset views

--- spec/features/user_password_reset_spec.rb
+++ spec/features/user_password_reset_spec.rb
@@ -6,12 +6,12 @@ RSpec.feature "UserPasswordResets", type: :feature do
   scenario 'valid password reset' do
     visit new_password_reset_path
     fill_in 'password_reset[email]', with: user.email
-    click_button I18n.t('password_resets.form.send_email')
+    click_button I18n.t('password_resets.new_form.submit')
     expect(page).to have_selector 'div#notice'
     visit password_reset_email_link
     fill_in 'user[password]', with: 'new_password'
     fill_in 'user[password_confirmation]', with: 'new_password'
-    click_button I18n.t('password_resets.form.password_reset')
+    click_button I18n.t('password_resets.edit_form.submit')
     expect(page).to have_selector 'div#notice'
   end
 end
