b97a62def3fc629429ee8915c04f69b695475fa2
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-04-15 01:29:13 +0900

    #44 add Comment views

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,20 @@
 module CommentsHelper
+  def create_comment_submit(form, text, options = {})
+    options[:class] = "p-2 border border-gray-400 text-black hover:text-white bg-emerald-400 hover:bg-emerald-500 rounded shadow #{options[:class]}"
+
+    form.submit(text, options)
+  end
+
+  def update_comment_submit(form, text, options = {})
+    options[:class] = "p-2 text-sm border border-gray-400 text-black hover:text-white bg-amber-400 hover:bg-amber-500 rounded shadow #{options[:class]}"
+
+    form.submit(text, options)
+  end
+
+  def destroy_comment_link_to(text, link, options = {})
+    options[:data] = { turbo_method: :delete, turbo_confirm: 'Are you sure?' }
+    options[:class] = "p-2 text-sm border border-gray-400 text-black hover:text-white bg-rose-400 hover:bg-rose-500 rounded shadow hover:no-underline #{options[:class]}"
+
+    link_to(text, link, options)
+  end
 end
---
+++ spec/features/comments_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Comments", type: :feature do
+  let!(:micropost) { create(:micropost) }
+  let!(:user)      { micropost.user }
+
+  scenario 'create comment' do
+    log_in_as(user)
+    visit micropost_path(micropost)
+    fill_in 'comment[content]', with: 'create comment!'
+    click_button I18n.t('comments.new_form.submit')
+    expect(page).to have_selector 'div#notice'
+    expect(page).to have_content 'create comment!'
+  end
+
+  scenario 'update comment' do
+    log_in_as(user)
+    user.comments.create!(content: 'create comment!', micropost_id: micropost.id)
+    visit micropost_path(micropost)
+    fill_in 'comment[content]', with: 'update comment!', match: :first
+    click_button I18n.t('comments.edit_form.update_submit')
+    expect(page).to have_selector 'div#notice'
+    expect(page).to have_content 'update comment!'
+  end
+
+  # scenario 'delete comment' do
+  #   log_in_as(user)
+  #   user.comments.create!(content: 'create comment!', micropost_id: micropost.id)
+  #   visit micropost_path(micropost)
+  #   click_link I18n.t('comments.edit_form.delete_submit')
+  #   expect(page).to have_selector 'div#notice'
+  #   expect(page).not_to have_content 'create comment!'
+  # end
+end
