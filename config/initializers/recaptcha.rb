9346a4986022c90c279a61eef0656c1675febe10
新参者 <73953416+shinzanmono@users.noreply.github.com>
2022-04-21 04:06:40 +0900

    recaptcha skip verify test

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,9 +2,9 @@ Recaptcha.configure do |config|
   config.site_key = ENV['RECAPTCHA_SITE_KEY']
   config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 
-  config.skip_verify_env += %w[development]
+  config.skip_verify_env += %w[development test]
 
-  if Rails.env.development?
+  if Rails.env.development? or Rails.env.test?
     config.site_key = ''
     config.secret_key = ''
   end
